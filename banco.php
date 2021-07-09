b06948accab1619417c9065433fc0dcdc138d9ff
williamtome <william.tomenh@gmail.com>
2021-07-09 19:10:35 -0300

    feat: add new account.

--- banco.php
+++ banco.php
@@ -2,6 +2,10 @@
 
 require_once 'src/Conta.php';
 
-$primeiraConta = new Conta();
+$primeiraConta = new Conta('William Tomé', '123.456.789-10');
 $primeiraConta->deposita(200);
-echo $primeiraConta->visualizaSaldo();
+echo $primeiraConta->visualizaDadosDoTitular();
+
+$segundaConta = new Conta('João das neves', '015.515.542-15');
+$segundaConta->deposita(300);
+echo $segundaConta->visualizaDadosDoTitular();
