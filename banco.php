9b630643a3e77e7553b0e828aab77e65fdbafa85
williamtome <william.tomenh@gmail.com>
2021-07-10 23:33:02 -0300

    refactor: add Titular instance on the constructor Conta class.

--- banco.php
+++ banco.php
@@ -3,11 +3,13 @@
 require_once 'src/Conta.php';
 require_once 'src/Titular.php';
 
-$primeiraConta = new Conta(new Titular('William Tomé', '123.456.789-10'));
+$william = new Titular('William Tomé', '123.456.789-10');
+$primeiraConta = new Conta($william);
 $primeiraConta->deposita(200);
 echo $primeiraConta->visualizaDadosDoTitular();
 
-$segundaConta = new Conta(new Titular('João das neves', '015.515.542-15'));
+$joao = new Titular('João das neves', '015.515.542-15');
+$segundaConta = new Conta($joao);
 $segundaConta->deposita(300);
 echo $segundaConta->visualizaDadosDoTitular();
 
