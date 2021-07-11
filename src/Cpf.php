444fe1edc39f62b8f64c8be2c5d64101b5989c0d
williamtome <william.tomenh@gmail.com>
2021-07-10 23:56:49 -0300

    feat: create Cpf class.

---
+++ src/Cpf.php
@@ -0,0 +1,28 @@
+<?php
+
+class Cpf
+{
+    private string $numero;
+
+    public function __construct(string $numero)
+    {
+        $this->validaCpf($numero);
+    }
+
+    public function recuperaNumero(): string
+    {
+        return $this->numero;
+    }
+
+    private function validaCpf(string $numero): void
+    {
+        if (strlen($numero) < 11) {
+            echo "CPF incorreto.";
+            exit();
+        }
+
+        $this->numero = $numero;
+    }
+
+
+}
