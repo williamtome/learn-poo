ec3ff84de4970a0c5317ef8b9d96ae59c7a88ad3
williamtome <william.tomenh@gmail.com>
2021-07-12 19:58:34 -0300

    refactor: add instance of Endereco class.

--- banco.php
+++ banco.php
@@ -3,13 +3,15 @@
 require_once 'src/Conta.php';
 require_once 'src/Titular.php';
 require_once 'src/Cpf.php';
+require_once 'src/Endereco.php';
 
-$william = new Titular('William Tomé', new Cpf('123.456.789-10'));
+$endereco = new Endereco('Clóvis Beviláqua', '945', 'um bairro', 'Canoas');
+$william = new Titular('William Tomé', new Cpf('123.456.789-10'), $endereco);
 $primeiraConta = new Conta($william);
 $primeiraConta->deposita(200);
 echo $primeiraConta->visualizaDadosDoTitular();
 
-$joao = new Titular('João das neves', new Cpf('015.515.542-15'));
+$joao = new Titular('João das neves', new Cpf('015.515.542-15'), $endereco);
 $segundaConta = new Conta($joao);
 $segundaConta->deposita(300);
 echo $segundaConta->visualizaDadosDoTitular();
