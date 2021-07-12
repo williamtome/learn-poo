31c965274b61e9c967e6af3efdaf6d5f7efe49f0
williamtome <william.tomenh@gmail.com>
2021-07-12 19:55:48 -0300

    feat: create Endereco class.

---
+++ src/Endereco.php
@@ -0,0 +1,37 @@
+<?php
+
+class Endereco
+{
+    private string $rua;
+    private string $numero;
+    private string $bairro;
+    private string $cidade;
+
+    public function __construct(string $rua, string $numero, string $bairro, string $cidade)
+    {
+        $this->rua = $rua;
+        $this->numero = $numero;
+        $this->bairro = $bairro;
+        $this->cidade = $cidade;
+    }
+
+    public function recuperaRua(): string
+    {
+        return $this->rua;
+    }
+
+    public function recuperaNumero(): string
+    {
+        return $this->numero;
+    }
+
+    public function recuperaBairro(): string
+    {
+        return $this->bairro;
+    }
+
+    public function recuperaCidade(): string
+    {
+        return $this->cidade;
+    }
+}
