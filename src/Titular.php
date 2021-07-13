38051f8057c3f62229b3169de2fee1abed8c432d
williamtome <william.tomenh@gmail.com>
2021-07-13 19:08:47 -0300

    refactor: Titular class extends PessoaFisica class and add Funcionario class.

--- src/Titular.php
+++ src/Titular.php
@@ -1,41 +1,15 @@
 <?php
 
-class Titular
+class Titular extends PessoaFisica
 {
-    private Cpf $cpf;
     private Endereco $endereco;
-    private string $nome;
+    private Funcionario $funcionario;
 
-    public function __construct(string $nome, Cpf $cpf, Endereco $endereco)
+    public function __construct(string $nome, Cpf $cpf, Endereco $endereco, Funcionario $funcionario)
     {
-        $this->validaNomeTitular($nome);
-        $this->cpf = $cpf;
+        parent::__construct($nome, $cpf);
         $this->endereco = $endereco;
-    }
-
-    public function recuperaNome(): string
-    {
-        return $this->nome;
-    }
-
-    public function recuperaCpf(): string
-    {
-        return $this->cpf->recuperaNumero();
-    }
-
-    private function validaNomeTitular(string $nome)
-    {
-        if (str_word_count($nome) == 0) {
-            echo "Informe o nome do titular." . PHP_EOL;
-            exit();
-        }
-
-        if (str_word_count($nome) == 1) {
-            echo "Informe o sobrenome do titular." . PHP_EOL;
-            exit();
-        }
-
-        $this->nome = $nome;
+        $this->funcionario = $funcionario;
     }
 
     public function recuperaEndereco(): Endereco
