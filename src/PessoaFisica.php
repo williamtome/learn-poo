fc03ec7c518065c4fb813440d02f8ddd9c98f31b
williamtome <william.tomenh@gmail.com>
2021-07-13 19:04:07 -0300

    feat: create PessoaFisica class.

---
+++ src/PessoaFisica.php
@@ -0,0 +1,38 @@
+<?php
+
+abstract class PessoaFisica
+{
+    protected string $nome;
+    protected Cpf $cpf;
+
+    public function __construct(string $nome, Cpf $cpf)
+    {
+        $this->validaNomeTitular($nome);
+        $this->cpf = $cpf;
+    }
+
+    public function recuperaNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function recuperaCpf(): string
+    {
+        return $this->cpf->recuperaNumero();
+    }
+
+    protected function validaNomeTitular(string $nome)
+    {
+        if (str_word_count($nome) == 0) {
+            echo "Informe o nome do titular." . PHP_EOL;
+            exit();
+        }
+
+        if (str_word_count($nome) == 1) {
+            echo "Informe o sobrenome do titular." . PHP_EOL;
+            exit();
+        }
+
+        $this->nome = $nome;
+    }
+}
