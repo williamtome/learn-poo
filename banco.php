bc455c8278eb35ed24754472029d821bc3c6468d
williamtome <william.tomenh@gmail.com>
2021-07-24 16:51:51 -0300

    refactor: remove funcionario object.

--- banco.php
+++ banco.php
@@ -7,13 +7,13 @@ use Alura\Banco\Modelo\{Cpf, Endereco, Funcionario};
 
 $endereco = new Endereco('Clóvis Beviláqua', '945', 'um bairro', 'Canoas');
 $funcionario = new Funcionario('William Tomé', new Cpf('123.456.789-10'), 'Desenvolvedor Back-end');
-$william = new Titular('William Tomé', new Cpf('123.456.789-10'), $endereco, $funcionario);
+$william = new Titular('William Tomé', new Cpf('123.456.789-10'), $endereco);
 $primeiraConta = new Conta($william);
 $primeiraConta->deposita(200);
 echo $primeiraConta->visualizaDadosDoTitular();
 
 $funcionario = new Funcionario('João das neves', new Cpf('015.515.542-15'), 'Auxiliar Administrativo');
-$joao = new Titular('João das neves', new Cpf('015.515.542-15'), $endereco, $funcionario);
+$joao = new Titular('João das neves', new Cpf('015.515.542-15'), $endereco);
 $segundaConta = new Conta($joao);
 $segundaConta->deposita(300);
 echo $segundaConta->visualizaDadosDoTitular();
--- src/Modelo/Conta/Titular.php
+++ src/Modelo/Conta/Titular.php
@@ -4,19 +4,16 @@ namespace Alura\Banco\Modelo\Conta;
 
 use Alura\Banco\Modelo\PessoaFisica;
 use Alura\Banco\Modelo\Endereco;
-use Alura\Banco\Modelo\Funcionario;
 use Alura\Banco\Modelo\Cpf;
 
 class Titular extends PessoaFisica
 {
     private Endereco $endereco;
-    private Funcionario $funcionario;
 
-    public function __construct(string $nome, Cpf $cpf, Endereco $endereco, Funcionario $funcionario)
+    public function __construct(string $nome, Cpf $cpf, Endereco $endereco)
     {
         parent::__construct($nome, $cpf);
         $this->endereco = $endereco;
-        $this->funcionario = $funcionario;
     }
 
     public function recuperaEndereco(): Endereco
