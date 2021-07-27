95ff3f495fac67c9bd14e95389b08af08476c898
williamtome <william.tomenh@gmail.com>
2021-07-27 08:45:02 -0300

    refactor: ContaCorrente object implemented.

--- teste-saque.php
+++ teste-saque.php
@@ -2,13 +2,13 @@
 
 require_once 'autoload.php';
 
-use \Alura\Banco\Modelo\Conta\Conta;
+use Alura\Banco\Modelo\Conta\ContaCorrente;
 use Alura\Banco\Modelo\Conta\ContaPoupanca;
-use \Alura\Banco\Modelo\Conta\Titular;
-use \Alura\Banco\Modelo\Cpf;
-use \Alura\Banco\Modelo\Endereco;
+use Alura\Banco\Modelo\Conta\Titular;
+use Alura\Banco\Modelo\Cpf;
+use Alura\Banco\Modelo\Endereco;
 
-$conta = new Conta(
+$conta = new ContaCorrente(
     new Titular(
         'William T.',
         new Cpf('212.456.899-10'),
@@ -30,5 +30,6 @@ $conta->saca(100);
 echo $conta->visualizaDadosDoTitular();
 
 $contaPoupanca->deposita(300);
+echo $contaPoupanca->visualizaDadosDoTitular();
 $contaPoupanca->saca(100);
 echo $contaPoupanca->visualizaDadosDoTitular();
