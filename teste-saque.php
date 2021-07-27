10894ae3ef671458364cf95196572f6219d1b4bb
williamtome <william.tomenh@gmail.com>
2021-07-27 15:00:02 -0300

    style: grouped classes in one namespace.

--- teste-saque.php
+++ teste-saque.php
@@ -2,10 +2,7 @@
 
 require_once 'autoload.php';
 
-use Alura\Banco\Modelo\Conta\ContaCorrente;
-use Alura\Banco\Modelo\Conta\ContaPoupanca;
-use Alura\Banco\Modelo\Conta\ContaSalario;
-use Alura\Banco\Modelo\Conta\Titular;
+use Alura\Banco\Modelo\Conta\{ContaCorrente, ContaPoupanca, ContaSalario, Titular};
 use Alura\Banco\Modelo\Cpf;
 use Alura\Banco\Modelo\Endereco;
 
