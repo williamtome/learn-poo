63c6ec25a1197375e943acf046480782792659ef
williamtome <william.tomenh@gmail.com>
2021-07-28 20:38:58 -0300

    feat: add salario property and calculate bonus employee method.

--- src/Modelo/Funcionario.php
+++ src/Modelo/Funcionario.php
@@ -5,11 +5,13 @@ namespace Alura\Banco\Modelo;
 class Funcionario extends PessoaFisica
 {
     private string $cargo;
+    private float $salario;
 
-    public function __construct(string $nome, Cpf $cpf, string $cargo)
+    public function __construct(string $nome, Cpf $cpf, string $cargo, float $salario)
     {
         parent::__construct($nome, $cpf);
         $this->cargo = $cargo;
+        $this->salario = $salario;
     }
 
     public function recuperaCargo(): string
@@ -20,5 +22,15 @@ class Funcionario extends PessoaFisica
     public function alteraNome(string $nome): void
     {
         $this->validaNomeTitular($nome);
+    }
+
+    public function recuperaSalario(): float
+    {
+        return $this->salario;
+    }
+
+    public function calculaBonificacao(): float
+    {
+        return $this->salario * 0.1;
     }
 }
