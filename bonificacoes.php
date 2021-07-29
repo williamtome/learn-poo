0a1856a11e666336f696118d252dca41ac10103d
williamtome <william.tomenh@gmail.com>
2021-07-29 20:40:36 -0300

    refactor: change instance of Funcionario by child class. Remove cargo property.

--- bonificacoes.php
+++ bonificacoes.php
@@ -1,27 +1,32 @@
 <?php
 
 use Alura\Banco\Modelo\Cpf;
-use Alura\Banco\Modelo\Funcionario;
+use Alura\Banco\Modelo\Funcionario\{Diretor, Gerente, Desenvolvedor};
 use Alura\Banco\Servico\ControladorDeBonificacoes;
 
 require_once 'autoload.php';
 
-$umFuncionario = new Funcionario(
+$umFuncionario = new Desenvolvedor(
     'William Tomé',
     new Cpf('012.345.678-90'),
-    'Desenvolvedor',
-    2000
+    1000
 );
 
 $umaFuncionaria = new Funcionario(
     'Débora V.',
     new Cpf('465.751.021-05'),
-    'Gerente',
+    3000
+);
+
+$diretor = new Diretor(
+    'Lucas C.',
+    new Cpf('777.722.044-98'),
     5000
 );
 
 $controlador = new ControladorDeBonificacoes();
 $controlador->adicionaBonificacaoDe($umFuncionario);
 $controlador->adicionaBonificacaoDe($umaFuncionaria);
+$controlador->adicionaBonificacaoDe($diretor);
 
 echo $controlador->recuperaTotal();
