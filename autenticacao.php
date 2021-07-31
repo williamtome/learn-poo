1b001b5738efaa68d2f022f8cb1b0880cda83f17
williamtome <william.tomenh@gmail.com>
2021-07-31 17:58:36 -0300

    feat: implemet Gerente and Titular objects.

--- autenticacao.php
+++ autenticacao.php
@@ -13,5 +13,19 @@ $umDiretor = new Diretor(
     6000
 );
 
+$umGerente = new Gerente(
+    'Cicrano V.',
+    new Cpf('174.333.924-01'),
+    9000
+);
+
+$umTitular = new Titular(
+    'Maria A.',
+    new Cpf('495.001.545-66'),
+    new Endereco('Calcaria', '90', 'Tal', 'Porto Alegre')
+);
+
 $autenticador = new Autenticador();
 $autenticador->tentaLogin($umDiretor, '1234');
+$autenticador->tentaLogin($umGerente, '4321');
+$autenticador->tentaLogin($umTitular, 'abcd');
