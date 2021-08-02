fa8ed0262812f6d8bd13603ead26274e8b45231c
williamtome <william.tomenh@gmail.com>
2021-08-02 20:25:59 -0300

    feat: change address data directly by attributes.

--- enderecos.php
+++ enderecos.php
@@ -7,4 +7,11 @@ require_once 'autoload.php';
 $umEndereco = new Endereco('Rua qualquer', '45b', 'qualquer coisa', 'Porto');
 $outroEndereco = new Endereco('Rua da lapa', '444', 'Qualquer', 'Petropolis');
 
+$umEndereco->rua = 'Rua Editada';
+$umEndereco->numero = 'Número Editado';
+$umEndereco->bairro = 'Bairro Editado';
+$umEndereco->cidade = 'Cidade editada';
+echo $umEndereco;
+exit();
+
 echo $umEndereco;
