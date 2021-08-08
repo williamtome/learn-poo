a98eb1cad4f23e5c9827b5965121861dc4c6e2a2
williamtome <william.tomenh@gmail.com>
2021-08-08 20:47:57 -0300

    feat: create custom exception class.

---
+++ src/Modelo/Conta/SaldoInsuficienteException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Alura\Banco\Modelo\Conta;
+
+class SaldoInsuficienteException extends \DomainException
+{
+    public function __construct(float $valorSaque, float $saldoAtual)
+    {
+        $message = "Você tentou sacar $valorSaque, mas tem apenas $saldoAtual em conta.";
+        parent::__construct($message);
+    }
+}
